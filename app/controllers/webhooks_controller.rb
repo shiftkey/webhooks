80991cecbd393c3b7b05530e4c38d6a1c5cbc6a6
Brendan Forster <github@brendanforster.com>
2019-11-17 19:38:09 -0400

    what about this?

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -5,14 +5,15 @@ class WebhooksController < ApplicationController
     event = request.headers['X-GitHub-Event']
     project = params[:project]
 
-    logger.info "Received event type '#{event}' for action #{project} at '#{Time.now}'"
+    logger.info "Received event type '#{event}' for project '#{project}' at '#{Time.now}'"
 
     verify_signature(params[:payload])
     :ok
   end
 
   def verify_signature(payload_body)
-    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['WEBHOOKS_SECRET_TOKEN'], payload_body)
-    return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.headers['HTTP_X_HUB_SIGNATURE'])
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['WEBHOOKS_SECRET_TOKEN'], payload_body
+
+    raise "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.headers['HTTP_X_HUB_SIGNATURE'])
   end
 end
