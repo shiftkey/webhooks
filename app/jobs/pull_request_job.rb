780c297a82fc021d7cbbf01e75a318cd9ad0a142
Brendan Forster <github@brendanforster.com>
2019-11-19 15:12:01 -0400

    tidy up a bit

--- app/jobs/pull_request_job.rb
+++ app/jobs/pull_request_job.rb
@@ -6,22 +6,28 @@ class PullRequestJob < ApplicationJob
 
     action = obj['action']
     pull_request_number = obj['number']
-    subject_id = obj['pull_request']['node_id']
-    base_sha = obj['pull_request']['base']['sha']
-    base_ref = obj['pull_request']['base']['ref']
-    head_sha = obj['pull_request']['head']['sha']
-    full_name = obj['pull_request']['head']['full_name']
-    default_branch = obj['pull_request']['base']['repo']['default_branch']
 
-    logger.info "Action '#{obj['action']}' for PR ##{pull_request_number} on repo '#{full_name}'"
+    pull_request = obj['pull_request']
+    base = pull_request['base']
+    head = pull_request['head']
+    subject_id = pull_request['node_id']
+
+    base_sha = base['sha']
+    base_ref = base['ref']
+    head_sha = head['sha']
+
+    repo = base['repo']
+    default_branch = repo['default_branch']
+
+    logger.info "Action '#{obj['action']}' for PR ##{pull_request_number} on repo '#{repo['full_name']}'"
 
     unless action == 'synchronize' || action == 'opened' || action == 'reopened'
       logger.info "Pull request action #{action} is not handled. Ignoring..."
       return
     end
 
-    unless base_ref == 'gh-pages'
-      logger.info "Pull request targets '#{base_ref}'' rather than the default branch. Ignoring..."
+    unless base_ref == default_branch
+      logger.info "Pull request targets '#{base_ref}' rather than the default branch '#{default_branch}'. Ignoring..."
       return
     end
 
