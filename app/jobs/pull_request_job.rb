a41327a803ef0c0abb8da88723f43b94c13b2dfa
Brendan Forster <github@brendanforster.com>
2019-11-19 14:43:36 -0400

    flesh out details that are in the PR

--- app/jobs/pull_request_job.rb
+++ app/jobs/pull_request_job.rb
@@ -3,6 +3,29 @@ class PullRequestJob < ApplicationJob
 
   def perform(payload)
     obj = JSON.parse(payload)
-    logger.info "Got pull_request event with action '#{obj['action']}'"
+
+    action = obj['action']
+    pull_request_number = obj['number']
+    subject_id = obj['pull_request']['node_id']
+    base_sha = obj['pull_request']['base']['sha']
+    base_ref = obj['pull_request']['base']['ref']
+    head_sha = obj['pull_request']['head']['sha']
+    full_name = obj['pull_request']['head']['full_name']
+    default_branch = obj['pull_request']['base']['repo']['default_branch']
+
+    logger.info "Action '#{obj['action']}' for PR ##{pull_request_number} on repo '#{full_name}'"
+
+    unless action == 'synchronize' || action == 'opened' || action == 'reopened'
+      logger.info "Pull request action #{action} is not handled. Ignoring..."
+      return
+    end
+
+    unless base_ref == 'gh-pages'
+      logger.info "Pull request targets '#{base_ref}'' rather than the default branch. Ignoring..."
+      return
+    end
+
+    logger.info "TODO: clone a repository to the temporary directory"
+    logger.info "TODO: find changes to files in diff #{base_sha}...#{head_sha}"
   end
 end
