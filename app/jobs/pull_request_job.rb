3586e3dc2a9be2b41677876a2930c80ac5c40b0a
Brendan Forster <github@brendanforster.com>
2019-11-23 20:23:45 -0400

    extract graphql checks and logic from main project

--- app/jobs/pull_request_job.rb
+++ app/jobs/pull_request_job.rb
@@ -67,8 +67,38 @@ class PullRequestJob < ApplicationJob
       end
 
       logger.info "Found files in this PR to process: '#{files}'"
-    end
 
+      http = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
+        def headers(_context)
+          {
+            "User-Agent": 'up-for-grabs-graphql-label-queries',
+            "Authorization": "bearer #{ENV['SHIFTBOT_GITHUB_TOKEN']}"
+          }
+        end
+      end
+
+      schema = GraphQL::Client.load_schema(http)
+
+      client = GraphQL::Client.new(schema: schema, execute: http)
+
+      cleanup_old_comments(client, pull_request_number)
+
+      schema = Pathname.new("#{dir}/schema.json")
+      schemer = JSONSchemer.schema(schema)
+
+      projects = files.map do |f|
+        full_path = File.join(dir, f)
+        return nil unless File.exist?(full_path)
+
+        Project.new(f, full_path)
+      end
+
+      markdown_body = generate_comment_for_pull_request(projects, schemer)
+
+      logger.info "Comment to submit: #{markdown_body}"
+
+      add_comment_to_pull_request(client, subject_id, markdown_body)
+    end
   end
 
   def run(cmd)
@@ -81,4 +111,124 @@ class PullRequestJob < ApplicationJob
       exit_code: status.exitstatus,
     }
   end
+
+  def cleanup_old_comments(client, pull_request_number)
+    Object.const_set :PullRequestComments, client.parse(<<-'GRAPHQL')
+      query ($owner: String!, $name: String!, $number: Int!) {
+        repository(owner: $owner, name: $name) {
+          pullRequest(number: $number) {
+            comments(first: 50) {
+              nodes {
+                id
+                body
+                author {
+                  login
+                  __typename
+                }
+              }
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    repo = ENV['GITHUB_REPOSITORY']
+    owner, name = repo.split('/')
+
+    variables = { owner: owner, name: name, number: pull_request_number }
+
+    response = client.query(PullRequestComments, variables: variables)
+
+    pull_request = response.data.repository.pull_request
+    comments = pull_request.comments
+
+    return unless comments.nodes.any?
+
+    Object.const_set :DeleteIssueComment, client.parse(<<-'GRAPHQL')
+      mutation ($input: DeleteIssueCommentInput!) {
+        deleteIssueComment(input: $input)
+      }
+    GRAPHQL
+
+    login = 'shiftbot'
+    type = 'User'
+    preamble = '<!-- PULL REQUEST ANALYZER GITHUB ACTION -->'
+
+    comments.nodes.each do |node|
+      author = node.author
+      match = author.login == login && author.__typename == type && node.body.include?(preamble)
+
+      next unless match
+
+      variables = { input: { id: node.id } }
+      response = client.query(DeleteIssueComment, variables: variables)
+
+      if response.errors.any?
+        message = response.errors[:data].join(', ')
+        logger.info "Message when deleting commit failed: #{message}"
+      end
+    end
+  end
+
+  def generate_comment_for_pull_request(projects, schemer)
+    markdown_body = "<!-- PULL REQUEST ANALYZER GITHUB ACTION -->
+    :wave: I'm a robot checking the state of this pull request to save the human reveiwers time." \
+    " I noticed this PR added or modififed the data files under `_data/projects/` so I had a look at what's changed.\n\n" \
+    "As you make changes to this pull request, I'll re-run these checks.\n\n"
+
+    messages = projects.compact.map { |p| review_project(p, schemer) }.map do |result|
+      path = result[:project].relative_path
+
+      if result[:kind] == 'valid'
+        "#### `#{path}` :white_check_mark: \nNo problems found, everything should be good to merge!"
+      elsif result[:kind] == 'validation'
+        message = result[:validation_errors].map { |e| "> - #{e}" }.join "\n"
+        "#### `#{path}` :x:\nI had some troubles parsing the project file, or there were fields that are missing that I need.\n\nHere's the details:\n#{message}"
+      elsif result[:kind] == 'repository' || result[:kind] == 'label'
+        "#### `#{path}` :x:\n#{result[:message]}"
+      else
+        "#### `#{path}` :question:\nI got a result of type '#{result[:kind]}' that I don't know how to handle. I need to mention @shiftkey here as he might be able to fix it."
+      end
+    end
+
+    markdown_body + messages.join("\n\n")
+  end
+
+  def add_comment_to_pull_request(client, subject_id, markdown_body)
+    Object.const_set :AddCommentToPullRequest, client.parse(<<-'GRAPHQL')
+      mutation ($input: AddCommentInput!) {
+        addComment(input: $input) {
+          commentEdge {
+            node {
+              url
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    variables = { input: { body: markdown_body, subjectId: subject_id } }
+
+    begin
+      response = client.query(AddCommentToPullRequest, variables: variables)
+
+      if (data = response.data)
+        if data.add_comment?
+          comment = data.add_comment.comment_edge.node
+          logger.info "A comment has been created at '#{comment.url}'"
+        elsif data.errors
+          logger.info 'Errors found in data from response:'
+          data.errors.each { |field, error| logger.info " - '#{field}' - #{error}" }
+        end
+      end
+
+      return unless response.errors.any?
+
+      logger.info 'Errors found in response when trying to add comment:'
+      response.errors.each { |field, error| logger.info " - '#{field}' - #{error}" }
+    rescue StandardError => e
+      logger.info "Unhandled exception occurred: #{e}"
+    end
+  end
+
 end
