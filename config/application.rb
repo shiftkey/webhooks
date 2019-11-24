6c06b017692b42a83d349af87d81c76231fbd647
Brendan Forster <github@brendanforster.com>
2019-11-24 14:56:57 -0400

    lib isn't included by default in autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module Webhooks
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # this directory is not included by default
+    config.autoload_paths += %W[#{config.root}/lib]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
